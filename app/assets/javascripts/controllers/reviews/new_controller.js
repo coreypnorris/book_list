51e339e71e88db58b9d30a089a67b4755cd915ba
Corey Norris <coreypnorris@gmail.com>
2014-05-10 22:36:37 -0700

    create form for creating new book reviews

---
+++ app/assets/javascripts/controllers/reviews/new_controller.js
@@ -0,0 +1,11 @@
+App.ReviewsNewController = Ember.Controller.extend({
+  ratings: [5,4,3,2,1],
+  actions: {
+    createReview: function() {
+      var controller = this;
+      this.get('model').save().then(function() {
+        controller.transitionToRoute('index');
+      });
+    }
+  }
+});
